95b86d519c7dde89e63469b0601dd0cc3361f4a5
Sean Chapinm <schapin7@gmail.com>
2019-09-10 13:40:57 -0600

    commit after getting sass running

--- application.py
+++ application.py
@@ -7,28 +7,21 @@ from flask_session import Session
 app = Flask(__name__)
 # app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app.config["SECRET KEY"] = 'secret key'
+app.config["SECRET KEY"] = os.urandom(24)
 # socketio = SocketIO(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
-''' @app.route("/clearSession", methods=["GET", "POST"])
-def clearSession():
-    if session["display_name"] != Null:
-        session.pop("display_name", None)
-        return 'Session is cleared!' 
-    return 'Session is already cleared' '''
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     display_name = request.form.get("name")
-    if session.get("display_name") == None:    
+    if session.get("display_name") == None:    # Check if display_name is already in the sessions dictionary
         session["display_name"] = display_name # Save the display_name in the session dictionary
         return  "Welcome " + display_name + "!"
     else:

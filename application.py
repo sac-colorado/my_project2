a2bda176c3d89b64012d65d4df099fbdd7b7ceba
Sean Chapinm <schapin7@gmail.com>
2019-09-30 12:12:01 -0600

    create_channel_list feature and the css for page formatting

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ app.config["SECRET KEY"] = os.urandom(24)
 
 # Global storage for chat room --> users, channels, messages
 user_names = []
-channel_list = [tst_channel1, tst_channel2, tst_channel3]
+channel_list = ["tst_channel1", "tst_channel2", "tst_channel3"]
 users_and_messages = {}
 
 
@@ -45,7 +45,7 @@ def login():
 
 @app.route("/list_current_channels", methods=["GET"])
 def list_current_channels():
-    return render_template("list_current_channels.html", channel_list = "channel_list")
+    return render_template("list_current_channels.html", channel_list = channel_list)
 
 @app.route("/create_new_channel", methods=["GET", "POST"])
 def create_new_channel():

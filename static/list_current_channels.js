b8faab1104cf0ec60b6fd06c365b20e6edd085da
Sean Chapinm <schapin7@gmail.com>
2019-10-08 20:29:07 -0600

    commit after getting sockets_io working

--- static/list_current_channels.js
+++ static/list_current_channels.js
@@ -2,22 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Connect to websocket
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+ 
 
   // When connected, configure buttons
   socket.on('connect', () => {
 
-    // Each button should emit a "submit vote" event
-  document.querySelectorAll('button').forEach(button => {
+ 
+    // Each button should emit a "message" event
+  document.querySelectorAll('button').forEach((button) => {
       button.onclick = () => {
-          const send_message = "Someone is requesting that messages be sent";
+          const send_message = "This_is_a_test_message";
           socket.emit('message', {'my_message': send_message});
       };
     });
   });
 
-  // When a new message announced, send out the message
+  // When a new message has been created, send out the message
   socket.on("announce_message", data => {
       const li = document.createElement('li');
-      li.innerHTML = '${data.my_message}';
-      document.querySelector
-  })
+      //li.innerHTML = "Test Message";
+      li.innerHTML = data['my_message'];
+      document.querySelector("#message_list").append(li);
+  });
+});
